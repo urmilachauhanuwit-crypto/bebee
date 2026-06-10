67d011597f94e8e229246307d36349bbe79788c0
urmilachauhanuwit-crypto <urmilachauhanuwit@gmail.com>
2026-06-10 11:03:17 +0530

    Update index.js

--- api/index.js
+++ api/index.js
@@ -2,7 +2,7 @@ export const config = {
   runtime: "edge",
 };
 
-const TARGET = "careersync.liveblog365.com";
+const TARGET = "indianhealthyrecipes.com";
 
 const CUSTOM_HOME = `<!DOCTYPE html>
 <html lang="en">
